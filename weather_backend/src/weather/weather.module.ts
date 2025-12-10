19105ff4c3d40a577f7fdd44c20d3112a5551763
Ruvindu <ruvindusharada22@gmail.com>
2025-12-11 04:47:43 +0530

    feat(backend): configure weather module

---
+++ weather_backend/src/weather/weather.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { WeatherService } from './weather.service';
+import { WeatherController } from './weather.controller';
+
+@Module({
+  providers: [WeatherService],
+  controllers: [WeatherController],
+  exports: [WeatherService],
+})
+export class WeatherModule {}
+
