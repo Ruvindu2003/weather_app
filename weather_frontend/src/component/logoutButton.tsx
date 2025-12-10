f874492f0b572d977a46762b459748c2d7c7ba43
Ruvindu <ruvindusharada22@gmail.com>
2025-12-11 04:49:43 +0530

    feat(frontend): logout component

---
+++ weather_frontend/src/component/logoutButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function LogoutButton() {
+    return (
+        <a
+            href="/api/auth/logout"
+            className="button logout"
+        >
+            Log Out
+        </a>
+    );
+}
